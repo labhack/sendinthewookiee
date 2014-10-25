8e9c60cef0352bbca8cea7689077ad348a03e41d
cimmone <cimmonezaldrin@gmail.com>
2014-10-25 18:36:37 -0400

    Initial code commit to share.

---
+++ src/test/java/martin/utils/AppTest.java
@@ -0,0 +1,254 @@
+package martin.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class AppTest {
+	static double[] teamMember1 = {30, 10, 20};
+	static double[] teamMember2 = {15, 20, 10};
+	static double[] teamMember3 = {50, 40, 25};
+	static double[] teamMember4 = {45, 40, 15};
+	static double[][] teamMembers = {teamMember1, teamMember2, teamMember3, teamMember4};
+	static double[][][] possibleDrillsArray = {{{5, 10, 10}, {20, 10, 5}, {10, 10, 10}, {25, 5, 5}},
+									{{5, 10, 10}, {20, 10, 5}, {10, 10, 10}, {25, 5, 5}},	
+									{{25, 5, 40}, {40, 40, 40}, {20, 40, 40}, {40, 40, 50}}
+									};
+	static List<double[][]> possibleDrills = Arrays.asList(possibleDrillsArray);
+	static final int[] timeForDrill = {3, 2, 3};
+	static final int skillsPerTeam = 3;
+	static final int numberOfTeams = 4;
+	static int minimum = 70;
+	static Random random = new Random();
+
+	
+	private static void reinitialize() {
+	}
+	private static void randomize() {
+		System.out.println("Team members:");
+		for (double[] teamMember: teamMembers) {
+			System.out.print("Team member 1: ");
+			for (int i = 0; i < teamMember.length; i++) {
+				teamMember[i] = random.nextInt(46) + 5; // 50 is max skill per team member
+				System.out.print(teamMember[i] + ",");
+			}
+			System.out.println();
+		}
+
+		System.out.println("Drills:");
+		for (double[][] possibleDrill: possibleDrillsArray) {
+			for (double[] impactPerSkill: possibleDrill) {
+				for (int i = 0; i < impactPerSkill.length; i++) {
+					impactPerSkill[i] = random.nextInt(36) + 5; // maximum of 40 impact per drill skill
+				}
+			}
+		}
+		
+		
+		
+	}
+	@Test
+	public void labhack() {
+		Steps bestPath = bestPath();
+		System.out.println("Best path: " + StringUtils.join(bestPath) + ", time taken: " + bestPath.getTimeTaken());
+	}
+
+	public static double distanceInAMinute (double value, int times, double original) {
+		return minimum - original - decay(value, times);
+	}
+	
+	public static double decay(double value, int times) {
+		return Math.pow(value, Math.pow(.95, times));
+	}
+	
+	public Steps bestPath() {
+		Steps bestSteps = new Steps();
+		int[] drillsUsedCounts = new int[possibleDrills.size()];
+		for (int i = 0; i < possibleDrills.size(); i++) {
+			drillsUsedCounts[i] = 0;
+		}
+
+		while (!bestSteps.status.isDone()) {
+			int bestDrill = bestDrill(drillsUsedCounts, bestSteps.status);
+			drillsUsedCounts[bestDrill]++;
+			bestSteps.add(bestDrill);
+		}
+		return bestSteps;
+	}
+	
+	public int bestDrill(int[] drillsUsedCounts, TeamStatus status) {
+		double lowestDistance = Double.MAX_VALUE;
+		int bestDrill = 0;
+		for (int i = 0; i < possibleDrills.size(); i++) {
+			System.out.print("Drill #: " + i + "{");
+			double averageDistancePerMinute = 0;
+			for (int j = 0; j < numberOfTeams; j++) {
+				System.out.print("{");
+				for (int k = 0; k < skillsPerTeam; k++) {
+					double[] drill = possibleDrills.get(i)[j];
+					double skill = drill[k];
+					double currentDistance = distanceInAMinute(skill, drillsUsedCounts[i] + 1, status.teamBySkill[j][k]);
+					averageDistancePerMinute += Math.max(0,  currentDistance * timeForDrill[i]);
+					System.out.printf("%.2f,", distanceInAMinute(skill, drillsUsedCounts[i] + 1, status.teamBySkill[j][k]));
+				}
+				System.out.print("}");
+			}
+			averageDistancePerMinute /= numberOfTeams * skillsPerTeam;
+			System.out.print("}" + averageDistancePerMinute + "\n");
+			// if so, we've found one that will finish the practice, go to special case
+			if (averageDistancePerMinute < Math.pow(10, -10)) {
+				// TODO special end case
+			}
+			if (averageDistancePerMinute < lowestDistance) {
+//				System.out.println("drill #: " + i + ", averageDistance: " + averageDistancePerMinute);
+				lowestDistance = averageDistancePerMinute;
+				bestDrill = i;
+			}
+		}
+		System.out.println();
+		return bestDrill;
+	}
+	
+	private static class Steps extends ArrayList<Integer> {
+		int timeTaken = 0;
+		TeamStatus status = new TeamStatus();
+		Steps() {
+			super();
+		}
+		public boolean add(Integer toAdd) {
+			timeTaken += timeForDrill[toAdd];
+			int drillCount = 1;
+			for (Integer step: this) {
+				if (step.equals(toAdd)) {
+					drillCount++;
+				}
+			}
+			status.add(stepAvailable(toAdd, drillCount));
+			return super.add(toAdd);
+		}
+		
+		
+		public double[][] stepAvailable (int drillNumber, int numberOfTimesUsed) {
+			double[][] drill = possibleDrills.get(drillNumber);
+			double[][] drillResult = newDrillResult();
+
+			for (int i = 0; i < drill.length; i++) {
+				for (int j = 0; j < drill[i].length; j++) {
+					drillResult[i][j] = decay(drill[i][j], numberOfTimesUsed);
+				}
+			}
+
+			return drillResult;
+		}
+		
+		public int getTimeTaken() {
+			return timeTaken;
+		}
+	}
+
+	private static double[][] newDrillResult() {
+		double[][] drillResult = new double[numberOfTeams][];
+
+		for (int i = 0; i < numberOfTeams; i++) {
+			drillResult[i] = new double[skillsPerTeam];
+			for (int j = 0; j < skillsPerTeam; j++) {
+				drillResult[i][j] = 0;
+			}
+		}
+
+		return drillResult;
+	}
+	private static class TeamStatus {
+		Double [][] teamBySkill;
+		TeamStatus() {
+			teamBySkill = new Double[numberOfTeams][];
+			for (int i = 0; i < numberOfTeams; i++) {
+				teamBySkill[i] = new Double [skillsPerTeam];
+				for (int j = 0; j < skillsPerTeam; j++) {
+					teamBySkill[i][j] = teamMembers[i][j];
+				}
+			}
+		}
+
+		void add (double[][] drillResult) {
+			for (int i = 0; i < teamBySkill.length; i++) {
+				for (int j = 0; j < teamBySkill[i].length; j++) {
+					teamBySkill[i][j] += drillResult[i][j];
+				}
+			}
+		}
+		public boolean isDone() {
+			for (Double[] skillset: teamBySkill) {
+				for (double skill: skillset) {
+					if (skill < minimum) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@Ignore
+	@Test
+	public void threeDice() {
+		Random random = new Random();
+		int numberOfRolls = 100000000;
+		int[] totalRolled = new int[19];
+		for (int i = 0; i < numberOfRolls; i++) {
+			int diceRolls = random.nextInt(5) + 2 + random.nextInt(5) + 2 + random.nextInt(5) + 2;
+			totalRolled[diceRolls]++;
+		}
+		System.out.println("----------Three dice----------");
+		double totalSoFar = 0;
+		for (int i = 6; i < totalRolled.length; i++) {
+			totalSoFar += totalRolled[i];
+			System.out.println("Number rolled: " + i + ", " + totalRolled[i] * 100.0 / numberOfRolls + "%, cumulative: " + totalSoFar / numberOfRolls * 100);
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void fourDice() {
+		int numberOfRolls = 100000000;
+		int[] totalRolled = new int[19];
+		for (int i = 0; i < numberOfRolls; i++) {
+			totalRolled[diceTotal()]++;
+		}
+		System.out.println("----------Four dice----------");
+		double totalSoFar = 0;
+		for (int i = 3; i < totalRolled.length; i++) {
+			totalSoFar += totalRolled[i];
+			System.out.println("Number rolled: " + i + ", " + totalRolled[i] * 100.0 / numberOfRolls + "%, cumulative: " + totalSoFar / numberOfRolls * 100);
+		}
+	}
+	
+	private int diceTotal() {
+		Random random = new Random();
+		int[] diceRolls = new int[4];
+		
+		for (int i = 0; i < diceRolls.length; i++) {
+			diceRolls[i] = random.nextInt(6) + 1;
+		}
+		
+		Arrays.sort(diceRolls);
+		int diceTotal = 0;
+		for (int i = 1; i < 4; i++) {
+			diceTotal += diceRolls[i];
+		}
+
+		return diceTotal;
+	}
+}
